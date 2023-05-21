430a8673c6b7b3b278e0adbdf0661859ec5d9925
kleberaluizio <kleberaluizio@gmail.com>
2023-05-21 17:31:53 -0300

    Starting to learn spring boot

---
+++ Spring-boot-example/src/main/java/com/kleberaluizio/SpringBootExampleApplication.java
@@ -0,0 +1,13 @@
+package com.kleberaluizio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+//288 lesson
+@SpringBootApplication
+public class SpringBootExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootExampleApplication.class, args);
+	}
+
+}
